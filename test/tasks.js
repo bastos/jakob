ba7723e52105c07715ccc3d69c17bb233c3a7ba6
Tiago Bastos <comechao@gmail.com>
2009-12-12 18:47:51 -0300

    Added namespace support

--- test/tasks.js
+++ test/tasks.js
@@ -1,19 +1,3 @@
 // To work without the jakob. prefix.
 process.mixin(jakob);
 
-task('example', function(options) {
-	x = 'Example task: ' + options['name'];
-	return x;
-});
-
-task('optionsname', function(options) {
-	return options['name'];
-});
-
-task('runwithoptions', function(options) {
-	return options['x'];
-});
-
-task('simple', function(options) {
-	return true;
-});
--- test/test.js
+++ test/test.js
@@ -1,5 +1,26 @@
+sys = require("sys");
 process.mixin(require("./common"));
+
+// Testing tasks using process.mixin:
 process.mixin(require("./tasks"));
+
+// Testing simple tasks:
+jakob.task('example', function(options) {
+	x = 'Example task: ' + options['name'];
+	return x;
+});
+
+jakob.task('optionsname', function(options) {
+	return options['name'];
+});
+
+jakob.task('runwithoptions', function(options) {
+	return options['x'];
+});
+
+jakob.task('simple', function(options) {
+	return true;
+});
 
 assert.equal(true, jakob.task('create', function(options){}), "Create a task");
 
@@ -21,10 +42,25 @@ assert.throws(function(){
 
 assert.equal('optionsname', jakob.invoke('optionsname'), "Has name on options");
 
-jakob.context('test', function() {
-	jakob.task('simplewithnamespace', function(options) {
+// Testing namespaces:
+
+jakob.namespace('test', function(test) {
+	test.task('simplewithnamespace', function(options) {
 		return true;
-	})
+	});
+	
+	test.namespace('namespace2', function(test) {
+		test.task('simplewithnamespace2', function(options) {
+			return true;
+		});		
+	});
+	
+	assert.equal(true, test.invoke(':simple'), "Called a task from the upper namespace");
+	
 });
 
+assert.equal(true, jakob.invoke('test:simplewithnamespace'), "Called a task with namespace");
+assert.equal(true, jakob.invoke('test:namespace2:simplewithnamespace2'), "Called a task with nested namespaces");
+
+// List all tasks:
 jakob.list();
