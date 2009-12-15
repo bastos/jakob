51ad8328d170d7078af32b17b5e799f3f7c42676
Tiago Bastos <comechao@gmail.com>
2009-12-15 00:29:23 -0300

    * Support for default task. * Using softabs now... * Jakob#namespaces is now Jakob#contexts cause a context is an instance of Jakob with an instance of Namespace.
    Ps. Maybe I need to think more about this namespace thing and contexts, but yaeah, I'm experimenting.

--- README.md
+++ README.md
@@ -5,47 +5,47 @@ How to use:
 
 Create a file Jakobfile.js and add tasks like this:
 
-	task('test', function(options) {
-		sys.puts("It Works");
-		return true;
-	});
+  task('test', function(options) {
+    sys.puts("It Works");
+    return true;
+  });
 
 Example:
 
-	$ jakob simple
-	>> It Works
+  $ jakob simple
+  >> It Works
 
 Namespaces:
 
-	namespace('test', function(test) {
-		test.task('simplewithnamespace', function(options) {
-			sys.puts("hey");
-			return true;
-		});
-	
-		test.namespace('namespace2', function(test) {
-			test.task('simplewithnamespace2', function(options) {
-				sys.puts("ho");			
-				return true;
-			});		
-		});
-	
-	});
-	
+  namespace('test', function(test) {
+    test.task('simplewithnamespace', function(options) {
+      sys.puts("hey");
+      return true;
+    });
+  
+    test.namespace('namespace2', function(test) {
+      test.task('simplewithnamespace2', function(options) {
+        sys.puts("ho");     
+        return true;
+      });   
+    });
+  
+  });
+  
 So type on your terminal:
 
-	$ jakob test:namespace2:simplewithnamespace2
-	>> ho
+  $ jakob test:namespace2:simplewithnamespace2
+  >> ho
 
 Dependencies:
 
-	jakob.task('runfirst', function(options) {
-		sys.puts("FIRST")
-	});
+  jakob.task('runfirst', function(options) {
+    sys.puts("FIRST")
+  });
 
-	jakob.task('second', function(options) {
-		sys.puts("SECOND")
-	}, { depends : ["runfirst"] })
+  jakob.task('second', function(options) {
+    sys.puts("SECOND")
+  }, { depends : ["runfirst"] })
 
 See more examples on test/test.js
 
@@ -57,5 +57,5 @@ How to Install?
 
 By now just do something like this:
 
-	export NODE_PATH=~/jakob/lib:$NODE_PATH
-	export PATH=~/jakob/bin:$PATH
+  export NODE_PATH=~/jakob/lib:$NODE_PATH
+  export PATH=~/jakob/bin:$PATH
--- test/test.js
+++ test/test.js
@@ -6,20 +6,20 @@ process.mixin(require("./tasks"));
 
 // Testing simple tasks:
 jakob.task('example', function(options) {
-	x = 'Example task: ' + options['name'];
-	return x;
+  x = 'Example task: ' + options['name'];
+  return x;
 });
 
 jakob.task('optionsname', function(options) {
-	return options['name'];
+  return options['name'];
 });
 
 jakob.task('runwithoptions', function(options) {
-	return options['x'];
+  return options['x'];
 });
 
 jakob.task('simple', function(options) {
-	return true;
+  return true;
 });
 
 assert.notEqual(false, jakob.task('create', function(options){}), "Create a task");
@@ -29,15 +29,15 @@ assert.equal('Example task: example', jakob.invoke('example'), "Simple task");
 assert.equal('optionsname', jakob.invoke('optionsname'), "Has name on options");
 
 assert.equal(10, jakob.invoke('runwithoptions', {x : 10}), "Test Options parameters");
-	
+  
 assert.throws(function(){
-	jakob.task('Name with spaces', function(options) {
-		return true;
-	});
+  jakob.task('Name with spaces', function(options) {
+    return true;
+  });
 }, JakobError);
 
 assert.throws(function(){
-	invoke('TaskDoesNotExists');
+  invoke('TaskDoesNotExists');
 }, JakobError);
 
 assert.equal('optionsname', jakob.invoke('optionsname'), "Has name on options");
@@ -45,18 +45,18 @@ assert.equal('optionsname', jakob.invoke('optionsname'), "Has name on options");
 // Testing namespaces:
 
 jakob.namespace('test', function(test) {
-	test.task('simplewithnamespace', function(options) {
-		return true;
-	});
-	
-	test.namespace('namespace2', function(test) {
-		test.task('simplewithnamespace2', function(options) {
-			return true;
-		});		
-	});
-	
-	assert.equal(true, test.invoke(':simple'), "Called a task from the upper namespace");
-	
+  test.task('simplewithnamespace', function(options) {
+    return true;
+  });
+  
+  test.namespace('namespace2', function(test) {
+    test.task('simplewithnamespace2', function(options) {
+      return true;
+    });   
+  });
+  
+  assert.equal(true, test.invoke(':simple'), "Called a task from the upper namespace");
+  
 });
 
 assert.equal(true, jakob.invoke('test:simplewithnamespace'), "Called a task with namespace");
@@ -67,12 +67,12 @@ assert.equal(true, jakob.invoke('test:namespace2:simplewithnamespace2'), "Called
 var runfirst_flag = false;
 
 jakob.task('runfirst', function(options) {
-	runfirst_flag = true;
-	return true;
+  runfirst_flag = true;
+  return true;
 });
 
 jakob.task('second', function(options) {
-	return true;
+  return true;
 }, { depends : ["runfirst"] })
 
 assert.equal(true, jakob.invoke('runfirst'), "Execute the second task");
